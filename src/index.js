40224c28e757687c067ff7ac25b2e7983e271c0d
Tarek Soliman <tarekadel89@gmail.com>
2025-09-06 19:57:46 +1000

    another trial

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-FHIR.oauth2.ready()
-    .then(client => client.request("Patient"))
-    .then(console.log)
-    .catch(console.error);
+// FHIR.oauth2.ready()
+//     .then(client => client.request("Patient"))
+//     .then(console.log)
+//     .catch(console.error);
